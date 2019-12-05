2d9814a2729dda288ceaa1dd37968e7cf225974e
Nadav Tasger <tashernadav@gmail.com>
2019-12-05 21:23:02 +0200

    Docs, Implementation improvements

--- scripts/backend/authenticate/api.php
+++ scripts/backend/authenticate/api.php
@@ -5,26 +5,30 @@
  * https://github.com/NadavTasher/AuthenticationTemplate/
  **/
 
-// Include base API
+// Include Base API
 include_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "base" . DIRECTORY_SEPARATOR . "api.php";
 
-// API hook name
+// API name
 const AUTHENTICATE_API = "authenticate";
 
-// General directory
+// Directories
 const AUTHENTICATE_DIRECTORY = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "files" . DIRECTORY_SEPARATOR . "authenticate";
-
-// Configuration file
-const AUTHENTICATE_CONFIGURATION_FILE = AUTHENTICATE_DIRECTORY . DIRECTORY_SEPARATOR . "configuration.json";
-
-// Sessions directory
 const AUTHENTICATE_SESSIONS_DIRECTORY = AUTHENTICATE_DIRECTORY . DIRECTORY_SEPARATOR . "sessions";
-
-// Names directory
 const AUTHENTICATE_NAMES_DIRECTORY = AUTHENTICATE_DIRECTORY . DIRECTORY_SEPARATOR . "names";
-
-// Users directory
 const AUTHENTICATE_USERS_DIRECTORY = AUTHENTICATE_DIRECTORY . DIRECTORY_SEPARATOR . "users";
+
+// Hooks configuration file
+const AUTHENTICATE_HOOKS_CONFIGURATION_FILE = AUTHENTICATE_DIRECTORY . DIRECTORY_SEPARATOR . "hooks.json";
+
+// Configuration constants
+
+const AUTHENTICATE_HASHING_ALGORITHM = "sha256";
+const AUTHENTICATE_HASHING_ROUNDS = 1024;
+const AUTHENTICATE_LENGTH_SALT = 512;
+const AUTHENTICATE_LENGTH_ID_SESSION = 512;
+const AUTHENTICATE_LENGTH_ID_USER = 20;
+const AUTHENTICATE_LENGTH_PASSWORD = 8;
+const AUTHENTICATE_LOCKOUT_TIMEOUT = 10;
 
 /**
  * This is the main API hook. It can be used by other APIs to handle authentication.
@@ -33,49 +37,51 @@ function authenticate()
 {
     // Return the result so that other APIs could use it.
     return api(AUTHENTICATE_API, function ($action, $parameters) {
-        $configuration = authenticate_configuration_load();
+        $configuration = authenticate_hooks_configuration_load();
         if ($configuration !== null) {
-            $hooks = $configuration->hooks;
-            if ($hooks->$action === true) {
-                if ($action === "authenticate") {
-                    // Authenticate the user using the session
-                    if (isset($parameters->session)) {
-                        return authenticate_session($parameters->session);
-                    }
-                    return [false, "Missing parameters", null];
-                } else if ($action === "signin") {
-                    // Authenticate the user using the password, return the new session
-                    if (isset($parameters->name) &&
-                        isset($parameters->password)) {
-                        $id = authenticate_name_load($parameters->name);
-                        if ($id !== null) {
-                            return authenticate_session_add($id, $parameters->password);
+            if (isset($configuration->$action)) {
+                if ($configuration->$action === true) {
+                    if ($action === "authenticate") {
+                        // Authenticate the user using the session
+                        if (isset($parameters->session)) {
+                            return authenticate_session($parameters->session);
                         }
-                        return [false, "User not found", null];
+                        return [false, "Missing parameters", null];
+                    } else if ($action === "signin") {
+                        // Authenticate the user using the password, return the new session
+                        if (isset($parameters->name) &&
+                            isset($parameters->password)) {
+                            $id = authenticate_name_load($parameters->name);
+                            if ($id !== null) {
+                                return authenticate_session_add($id, $parameters->password);
+                            }
+                            return [false, "User not found", null];
+                        }
+                        return [false, "Missing parameters", null];
+                    } else if ($action === "signup") {
+                        // Create a new user
+                        if (isset($parameters->name) &&
+                            isset($parameters->password)) {
+                            return authenticate_user_add($parameters->name, $parameters->password);
+                        }
+                        return [false, "Missing parameters", null];
                     }
-                    return [false, "Missing parameters", null];
-                } else if ($action === "signup") {
-                    // Create a new user
-                    if (isset($parameters->name) &&
-                        isset($parameters->password)) {
-                        return authenticate_user_add($parameters->name, $parameters->password);
-                    }
-                    return [false, "Missing parameters", null];
                 }
+                return [false, "Locked hook", null];
             }
-            return [false, "Locked hook", null];
+            return [false, "Undefined hook", null];
         }
         return [false, "Failed to load configuration", null];
     }, true);
 }
 
 /**
- * This function loads the configuration.
- * @return stdClass Configuration
+ * This function loads the hooks configurations.
+ * @return stdClass Hooks Configuration
  */
-function authenticate_configuration_load()
+function authenticate_hooks_configuration_load()
 {
-    return json_decode(file_get_contents(AUTHENTICATE_CONFIGURATION_FILE));
+    return json_decode(file_get_contents(AUTHENTICATE_HOOKS_CONFIGURATION_FILE));
 }
 
 /**
@@ -91,7 +97,7 @@ function authenticate_session_load($session)
 }
 
 /**
- * This function saves a session by its $session id.
+ * This function saves a session by its id.
  * @param string $session Session ID
  * @param string $id User ID
  */
@@ -101,7 +107,7 @@ function authenticate_session_unload($session, $id)
 }
 
 /**
- * This function loads a User ID by its $name name.
+ * This function loads a User ID by its name.
  * @param string $name User Name
  * @return string User ID
  */
@@ -113,7 +119,7 @@ function authenticate_name_load($name)
 }
 
 /**
- * This function saves a User ID by its $name name.
+ * This function saves a User ID by its name.
  * @param string $name User Name
  * @param string $id User ID
  */
@@ -123,7 +129,7 @@ function authenticate_name_unload($name, $id)
 }
 
 /**
- * This function loads a user by its $id id.
+ * This function loads a user by its id.
  * @param string $id User ID
  * @return stdClass User
  */
@@ -136,7 +142,7 @@ function authenticate_user_load($id)
 }
 
 /**
- * This function saves a user by its $id id.
+ * This function saves a user by its id.
  * @param string $id User ID
  * @param stdClass $user User
  */
@@ -146,30 +152,27 @@ function authenticate_user_unload($id, $user)
 }
 
 /**
- * This function authenticates the user using $id and $password, then returns the User's ID.
+ * This function authenticates a user using $id and $password, then returns a User ID.
  * @param string $id User ID
  * @param string $password User Password
  * @return array Action Result
  */
 function authenticate_user($id, $password)
 {
-    $configuration = authenticate_configuration_load();
     $user = authenticate_user_load($id);
-    if ($configuration !== null) {
-        if ($user !== null) {
-            if ($user->security->lock->time < time()) {
-                if (authenticate_hash_salted($password, $user->security->password->salt) === $user->security->password->hashed) {
-                    return [true, null, null];
-                }
-                $user->security->lock->time = time() + $configuration->security->lock;
+    if ($user !== null) {
+        if ($user->security->lockout->time < time()) {
+            if (authenticate_hash_salted($password, $user->security->password->salt) === $user->security->password->hashed) {
+                return [true, null, null];
+            } else {
+                $user->security->lockout->time = time() + AUTHENTICATE_LOCKOUT_TIMEOUT;
                 authenticate_user_unload($id, $user);
                 return [false, "Wrong password", null];
             }
-            return [false, "User is locked", null];
         }
-        return [false, "Failed loading user", null];
+        return [false, "User is locked", null];
     }
-    return [false, "Failed loading configuration", null];
+    return [false, "Failed loading user", null];
 }
 
 /**
@@ -180,40 +183,36 @@ function authenticate_user($id, $password)
  */
 function authenticate_user_add($name, $password)
 {
-    $configuration = authenticate_configuration_load();
-    if ($configuration !== null) {
-        // Check user name
-        if (authenticate_name_load($name) === null) {
-            // Check password length
-            if (strlen($password) >= $configuration->security->password) {
-                // Generate a unique user id
-                $id = random($configuration->security->id);
-                while (authenticate_user_load($id) !== null)
-                    $id = random($configuration->security->id);
-                // Initialize the user
-                $user = new stdClass();
-                $user->name = $name;
-                $user->security = new stdClass();
-                $user->security->password = new stdClass();
-                $user->security->password->salt = random($configuration->security->salt);
-                $user->security->password->hashed = authenticate_hash_salted($password, $user->security->password->salt);
-                $user->security->lock = new stdClass();
-                $user->security->lock->time = 0;
-                // Save user
-                authenticate_name_unload($name, $id);
-                authenticate_user_unload($id, $user);
-                return [true, null, null];
+    // Check user name
+    if (authenticate_name_load($name) === null) {
+        // Check password length
+        if (strlen($password) >= AUTHENTICATE_LENGTH_PASSWORD) {
+            // Generate a unique user id
+            $id = random(AUTHENTICATE_LENGTH_ID_USER);
+            while (authenticate_user_load($id) !== null)
+                $id = random(AUTHENTICATE_LENGTH_ID_USER);
+            // Initialize the user
+            $user = new stdClass();
+            $user->name = $name;
+            $user->security = new stdClass();
+            $user->security->password = new stdClass();
+            $user->security->password->salt = random(AUTHENTICATE_LENGTH_SALT);
+            $user->security->password->hashed = authenticate_hash_salted($password, $user->security->password->salt);
+            $user->security->lockout = new stdClass();
+            $user->security->lockout->time = 0;
+            // Save user
+            authenticate_name_unload($name, $id);
+            authenticate_user_unload($id, $user);
+            return [true, null, null];
 
-            }
-            return [false, "Password too short", null];
         }
-        return [false, "User already exists", null];
+        return [false, "Password too short", null];
     }
-    return [false, "Failed loading configuration", null];
+    return [false, "User already exists", null];
 }
 
 /**
- * This function authenticates the user using $session then returns the User's ID.
+ * This function authenticates a user using $session then returns a User ID.
  * @param string $session Session
  * @return array Action Result
  */
@@ -227,48 +226,37 @@ function authenticate_session($session)
 }
 
 /**
- * This function authenticates the user and creates a new session.
+ * This function authenticates a user and creates a new session for that user.
  * @param string $id User ID
  * @param string $password User Password
  * @return array Action Result
  */
 function authenticate_session_add($id, $password)
 {
-    $configuration = authenticate_configuration_load();
-    if ($configuration !== null) {
-        $authentication = authenticate_user($id, $password);
-        if ($authentication[0]) {
-            $session = random($configuration->security->session);
-            authenticate_session_unload($session, $id);
-            return [true, $session, null];
-        }
-        return $authentication;
+    $authentication = authenticate_user($id, $password);
+    if ($authentication[0]) {
+        $session = random(AUTHENTICATE_LENGTH_ID_SESSION);
+        authenticate_session_unload($session, $id);
+        return [true, $session, null];
     }
-    return [false, "Failed loading configuration", null];
+    return $authentication;
 }
 
 /**
  * This function hashes a secret.
  * @param string $secret Secret
- * @param int $onion Number of layers to hash
+ * @param int $rounds Number of rounds to hash
  * @return string Hashed
  */
-function authenticate_hash($secret, $onion = null)
+function authenticate_hash($secret, $rounds = AUTHENTICATE_HASHING_ROUNDS)
 {
-    // Load configuration
-    $configuration = authenticate_configuration_load();
-    // Initialize algorithm
-    $algorithm = $configuration->security->algorithm;
-    // Initialize onion if null
-    if ($onion === null)
-        $onion = $configuration->security->rounds;
     // Layer > 0 result
-    if ($onion > 0) {
-        $layer = authenticate_hash($secret, $onion - 1);
-        $return = hash($algorithm, $layer);
+    if ($rounds > 0) {
+        $layer = authenticate_hash($secret, $rounds - 1);
+        $return = hash(AUTHENTICATE_HASHING_ALGORITHM, $layer);
     } else {
         // Layer 0 result
-        $return = hash($algorithm, $secret);
+        $return = hash(AUTHENTICATE_HASHING_ALGORITHM, $secret);
     }
     return $return;
 }
@@ -277,25 +265,18 @@ function authenticate_hash($secret, $onion = null)
  * This function hashes a secret with a salt.
  * @param string $secret Secret
  * @param string $salt Salt
- * @param int $onion Number of layers to hash
+ * @param int $rounds Number of rounds to hash
  * @return string Hashed
  */
-function authenticate_hash_salted($secret, $salt, $onion = null)
+function authenticate_hash_salted($secret, $salt, $rounds = AUTHENTICATE_HASHING_ROUNDS)
 {
-    // Load configuration
-    $configuration = authenticate_configuration_load();
-    // Initialize algorithm
-    $algorithm = $configuration->security->algorithm;
-    // Initialize onion if null
-    if ($onion === null)
-        $onion = $configuration->security->onionLayers;
     // Layer > 0 result
-    if ($onion > 0) {
-        $layer = authenticate_hash_salted($secret, $salt, $onion - 1);
-        $return = hash($algorithm, ($onion % 2 === 0 ? $layer . $salt : $salt . $layer));
+    if ($rounds > 0) {
+        $layer = authenticate_hash_salted($secret, $salt, $rounds - 1);
+        $return = hash(AUTHENTICATE_HASHING_ALGORITHM, ($rounds % 2 === 0 ? $layer . $salt : $salt . $layer));
     } else {
         // Layer 0 result
-        $return = hash($algorithm, $secret . $salt);
+        $return = hash(AUTHENTICATE_HASHING_ALGORITHM, $secret . $salt);
     }
     return $return;
 }
