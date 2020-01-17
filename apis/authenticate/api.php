70fc04f8bd62c7b4096d7fef2ad54cf98190f623
Nadav Tasher <tashernadav@gmail.com>
2020-01-17 22:51:44 +0200

    WAB update Database update

--- apis/authenticate/api.php
+++ apis/authenticate/api.php
@@ -28,6 +28,8 @@ class Authenticate
     private const LENGTH_PASSWORD = 8;
     // Lock timeout
     private const TIMEOUT_LOCK = 10;
+    // Database
+    private static $database = null;
 
     /**
      * Main API hook. Can be used by other APIs to handle authentication.
@@ -35,11 +37,12 @@ class Authenticate
     public static function init()
     {
         // Make sure the database is initiated.
-        Database::create();
-        Database::create_column(self::COLUMN_NAME);
-        Database::create_column(self::COLUMN_SALT);
-        Database::create_column(self::COLUMN_HASH);
-        Database::create_column(self::COLUMN_LOCK);
+        self::$database = new Database(__DIR__);
+        self::$database->create();
+        self::$database->create_column(self::COLUMN_NAME);
+        self::$database->create_column(self::COLUMN_SALT);
+        self::$database->create_column(self::COLUMN_HASH);
+        self::$database->create_column(self::COLUMN_LOCK);
         // Return the result so that other APIs could use it.
         return API::handle(self::API, function ($action, $parameters) {
             $configuration = self::hooks();
@@ -61,7 +64,7 @@ class Authenticate
                                 isset($parameters->password)) {
                                 if (is_string($parameters->name) &&
                                     is_string($parameters->password)) {
-                                    if (count($ids = Database::search(self::COLUMN_NAME, $parameters->name)) === 1) {
+                                    if (count($ids = self::$database->search(self::COLUMN_NAME, $parameters->name)) === 1) {
                                         return self::session_add($ids[0], $parameters->password);
                                     }
                                     return [false, "User not found", null];
@@ -109,21 +112,21 @@ class Authenticate
     private static function user($id, $password)
     {
         // Check if the user's row exists
-        if (Database::has_row($id)) {
+        if (self::$database->has_row($id)) {
             // Retrieve the lock value
-            $lock = intval(Database::get($id, self::COLUMN_LOCK));
+            $lock = intval(self::$database->get($id, self::COLUMN_LOCK));
             // Verify that the user isn't locked
             if ($lock < time()) {
                 // Retrieve the salt and hash
-                $salt = Database::get($id, self::COLUMN_SALT);
-                $hash = Database::get($id, self::COLUMN_HASH);
+                $salt = self::$database->get($id, self::COLUMN_SALT);
+                $hash = self::$database->get($id, self::COLUMN_HASH);
                 // Check password match
                 if (self::hash_salted($password, $salt) === $hash) {
                     // Return a success result
                     return [true, null, null];
                 } else {
                     // Lock the user
-                    Database::set($id, self::COLUMN_LOCK, strval(time() + self::TIMEOUT_LOCK));
+                    self::$database->set($id, self::COLUMN_LOCK, strval(time() + self::TIMEOUT_LOCK));
                     // Return a failure result
                     return [false, "Wrong password", null];
                 }
@@ -144,19 +147,19 @@ class Authenticate
     private static function user_add($name, $password)
     {
         // Check user name
-        if (count(Database::search(self::COLUMN_NAME, $name)) === 0) {
+        if (count(self::$database->search(self::COLUMN_NAME, $name)) === 0) {
             // Check password length
             if (strlen($password) >= self::LENGTH_PASSWORD) {
                 // Generate a unique user id
-                $id = Database::create_row();
+                $id = self::$database->create_row();
                 // Generate salt and hash
                 $salt = self::random(self::LENGTH_SALT);
                 $hash = self::hash_salted($password, $salt);
                 // Set user information
-                Database::set($id, self::COLUMN_NAME, $name);
-                Database::set($id, self::COLUMN_SALT, $salt);
-                Database::set($id, self::COLUMN_HASH, $hash);
-                Database::set($id, self::COLUMN_LOCK, strval("0"));
+                self::$database->set($id, self::COLUMN_NAME, $name);
+                self::$database->set($id, self::COLUMN_SALT, $salt);
+                self::$database->set($id, self::COLUMN_HASH, $hash);
+                self::$database->set($id, self::COLUMN_LOCK, strval("0"));
                 // Return a success result
                 return [true, null, null];
             }
@@ -175,9 +178,9 @@ class Authenticate
     private static function session($session)
     {
         // Check if a link with the session's hash value
-        if (Database::has_link(self::hash($session))) {
+        if (self::$database->has_link(self::hash($session))) {
             // Return a success result with a server result of the user's ID
-            return [true, null, Database::follow_link(self::hash($session))];
+            return [true, null, self::$database->follow_link(self::hash($session))];
         }
         // Fallback result
         return [false, "Invalid session", null];
@@ -198,7 +201,7 @@ class Authenticate
             // Generate a new session ID
             $session = self::random(self::LENGTH_SESSION);
             // Create a database link with the session's hash
-            Database::create_link($id, self::hash($session));
+            self::$database->create_link($id, self::hash($session));
             // Return a success result
             return [true, $session, null];
         }
@@ -253,7 +256,7 @@ class Authenticate
     private static function random($length = 0)
     {
         if ($length > 0) {
-            return str_shuffle("0123456789abcdefghijklmnopqrstuvwxyz")[0] . Database::id($length - 1);
+            return str_shuffle("0123456789abcdefghijklmnopqrstuvwxyz")[0] . self::$database->id($length - 1);
         }
         return "";
     }
